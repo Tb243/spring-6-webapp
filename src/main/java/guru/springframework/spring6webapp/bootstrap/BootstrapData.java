50c025fd513fed02de3352f3668fcc21889fa2b1
tom <blakely86@live.com.au>
2024-05-01 17:52:52 +1000

    adding bootstrap data

---
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -0,0 +1,41 @@
+package guru.springframework.spring6webapp.bootstrap;
+
+import guru.springframework.spring6webapp.domain.Author;
+import guru.springframework.spring6webapp.domain.Book;
+import guru.springframework.spring6webapp.repositories.AuthorRepository;
+import guru.springframework.spring6webapp.repositories.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootstrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Author John = new Author();
+        John.setFirstName("John");
+        John.setLastName("Smith");
+
+        Book SFG = new Book();
+        SFG.setTitle("Spring Framework Guru");
+        SFG.setIsbn("123");
+
+        Author JohnSaved = authorRepository.save(John);
+        Book SFGSaved = bookRepository.save(SFG);
+
+        JohnSaved.getBooks().add(SFGSaved);
+
+        System.out.println("In bootstrapping data");
+        System.out.println("Author count: " + authorRepository.count());
+        System.out.println("Book count: " + bookRepository.count());
+
+    }
+}
