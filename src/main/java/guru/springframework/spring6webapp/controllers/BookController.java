59a03add3d8e2ee05ea67e88e24b0b5c897c6423
tom <blakely86@live.com.au>
2025-02-03 14:07:29 +1100

    added getbooks

--- src/main/java/guru/springframework/spring6webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring6webapp/controllers/BookController.java
@@ -2,6 +2,7 @@ package guru.springframework.spring6webapp.controllers;
 
 import guru.springframework.spring6webapp.services.BookService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 
 @Controller
 public class BookController {
@@ -11,4 +12,11 @@ public class BookController {
     public BookController(BookService bookService) {
         this.bookService = bookService;
     }
+
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookService.findAll());
+
+        return "books";
+    }
+
 }
